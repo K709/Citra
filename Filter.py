d652eb08bb408f3b2e6c2f39fcd95d7efe2065c1
Lala <lauralaurensiasim@gmail.com>
2022-12-01 17:32:40 +0700

    Update Frame | BERHASIL!!!!

---
+++ Filter.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+
+def pencil(img):
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    neg = 255-gray
+    blur = cv.GaussianBlur(neg,ksize=(21,21),sigmaX=0,sigmaY=0)
+
+    dodge = cv.divide(gray,255-blur,scale=256)
+    burn = 255-cv.divide(255-dodge,255-blur,scale=256)
+
+    return burn
+
+def cartoonize(img):
+    img_grey  = cv.cvtColor(img,cv.COLOR_RGB2GRAY)
+    img_blur = cv.medianBlur(img_grey,3)
+    img_bb = cv.bilateralFilter(img, 15, 75,75)
+    edges = cv.adaptiveThreshold(img_blur,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 3, 3)
+
+    imgf=np.float32(img_bb).reshape(-1,3)
+    criteria=(cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,20,1.0)
+    compactness,label,center=cv.kmeans(imgf,5,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
+    center=np.uint8(center)
+    final_img=center[label.flatten()]
+    final_img=final_img.reshape(img_bb.shape)
+    final=cv.bitwise_and(final_img,final_img,mask=edges)
+
+    return final
+
+def sephia(img):
+    print
+
+def anaglyph(img):
+    add_red = np.full([img.shape[0],img.shape[1],3],[0,0,255], 'uint8')
+    add_blue = np.full([img.shape[0],img.shape[1],3],[255,0,0], 'uint8')
+    slide = 20
+
+    red = img.copy()
+    red = red[:, slide:red.shape[1]]
+    red[:,:,0] = red[:,:,1] = 0
+    red_copy = add_red.copy()
+    red_copy[:red.shape[0],:red.shape[1]] = red
+
+    blue = img.copy()
+    blue = blue[:, 0:blue.shape[1]-slide]
+    blue[:,:,1] = blue[:,:,2] = 0
+    blue_copy = add_blue.copy()
+    blue_copy[0:img.shape[0],slide:img.shape[1]] = blue
+
+    green = img.copy()
+    green[:,:,0] = green[:,:,2] = 0
+
+    multiply = cv.bitwise_or(green,red_copy)
+    multiply = cv.bitwise_or(multiply,blue_copy)
+
+    return multiply
--- GUI.py
+++ GUI.py
@@ -1,94 +1,131 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import Tools
+import Filter
+import numpy as np
 
-img = None
+ori_img = None
+filter_img = None
 #Create The Method to Call Method in Another Class
+
+def createFrame():
+    global root, ori_img, filter_img
+
+    root = Tk()
+    root.title("Image Filter Buset")  # title of the GUI window
+    root.maxsize(1000, 600)  # specify the max size the window can expand to
+    root.config(bg="skyblue")  # specify background color
+
+    # Create left and right frames
+    left_frame = Frame(root, width=200, height=400, bg='grey')
+    left_frame.grid(row=0, column=0, padx=10, pady=5)
+
+    right_frame = Frame(root, width=650, height=400, bg='grey')
+    right_frame.grid(row=0, column=1, padx=10, pady=5)
+    
+    img = filter_img
+    if(img != None):
+        # Create frames and labels in left_frame
+        Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
+
+        
+        # load image to be "edited"
+        width, height = img.size
+        ori_width, ori_height = img.size
+
+        while(width>650 or height>400):
+            img = img.resize((int(width/2),int(height/2)))
+            width, height = img.size
+            print(width, height)
+
+        image = ImageTk.PhotoImage(img)
+        original_image = ImageTk.PhotoImage(ori_img.resize((int(width/2),int(height/2))))  # resize image using subsample
+        Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
+
+        # Display image in right_frame
+        Label(right_frame, image=image).grid(row=0,column=0, padx=5, pady=5)
+
+    # Create tool bar frame
+    tool_bar = Frame(left_frame, width=180, height=185)
+    tool_bar.grid(row=2, column=0, padx=5, pady=5)
+
+    # Example labels that serve as placeholders for other widgets
+    Label(tool_bar, text="Tools", width=15).grid(row=0, column=0, padx=5, pady=3)  # ipadx is padding inside the Label widget
+    Label(tool_bar, text="Filters", width=15).grid(row=0, column=1, padx=5, pady=3)
+
+    # Example labels that could be displayed under the "Tool" menu
+    Button(tool_bar, text="Select An Image", command=select, width=15).grid(row=1, column=0, padx=5, pady=5)
+    Button(tool_bar, text="Take A Picture", command=picture, width=15).grid(row=2, column=0, padx=5, pady=5)
+
+    if(img != None):
+        Button(tool_bar, text="Export", command=export, width=15).grid(row=3, column=0, padx=5, pady=5)
+
+        # Example labels that could be displayed under the "Filter" menu
+        Button(tool_bar, text="Pencil Sketch", command=pencil, width=15).grid(row=1, column=1, padx=5, pady=5)
+        Button(tool_bar, text="Cartoonize", command=cartoonize, width=15).grid(row=2, column=1, padx=5, pady=5)
+        Button(tool_bar, text="Sephia", command=sephia, width=15).grid(row=3, column=1, padx=5, pady=5)
+        Button(tool_bar, text="Anaglyph", command=anaglyph, width=15).grid(row=4, column=1, padx=5, pady=5)
+
+    else:
+        b1 = Button(tool_bar, text="Export", command="", width=15)
+        b1['state'] = DISABLED
+        b1.grid(row=3, column=0, padx=5, pady=5)
+        
+        # Example labels that could be displayed under the "Filter" menu
+        b2 = Button(tool_bar, text="Pencil Sketch", command="", width=15)
+        b2['state'] = DISABLED
+        b2.grid(row=1, column=1, padx=5, pady=5)
+
+        b3 = Button(tool_bar, text="Cartoonize", command="", width=15)
+        b3['state'] = DISABLED
+        b3.grid(row=2, column=1, padx=5, pady=5)
+
+        b4 = Button(tool_bar, text="Sephia", command="", width=15)
+        b4['state'] = DISABLED
+        b4.grid(row=3, column=1, padx=5, pady=5)
+
+        b5 = Button(tool_bar, text="Anaglyph", command="", width=15)
+        b5['state'] = DISABLED
+        b5.grid(row=4, column=1, padx=5, pady=5)
+
+    root.mainloop()
+
 def select():
+    global ori_img, root, filter_img
     img_path = Tools.browseFiles()
-    img = Image.open(img_path)
-    
-    
+    ori_img = Image.open(img_path)
+    filter_img = ori_img
+    root.destroy()
+    createFrame()
+
 def picture():
     picture = ""
 
 def export():
     export = ""
 
+def pencil():
+    global filter_img, root, ori_img
+    img = np.array(ori_img)
+    filter_img = Image.fromarray(Filter.pencil(img))
+    root.destroy()
+    createFrame()
 
-# Create The GUI
-root = Tk()  # create root window
-root.title("Image Filter Buset")  # title of the GUI window
-root.maxsize(900, 600)  # specify the max size the window can expand to
-root.config(bg="skyblue")  # specify background color
+def cartoonize():
+    global filter_img, root, ori_img
+    img = np.array(ori_img)
+    filter_img = Image.fromarray(Filter.cartoonize(img))
+    root.destroy()
+    createFrame()
 
-# Create left and right frames
-left_frame = Frame(root, width=200, height=400, bg='grey')
-left_frame.grid(row=0, column=0, padx=10, pady=5)
+def sephia():
+    print
 
-right_frame = Frame(root, width=650, height=400, bg='grey')
-right_frame.grid(row=0, column=1, padx=10, pady=5)
-
-if(img != None):
-    # Create frames and labels in left_frame
-    Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
-
-    # load image to be "edited"
-    width, height = img.size
-
-    while(width>650 or height>400):
-        img = img.resize((int(width/2),int(height/2)))
-        width, height = img.size
-        print(width, height)
-
-    image = ImageTk.PhotoImage(img)
-    original_image = ImageTk.PhotoImage(img.resize((int(width/2),int(height/2))))  # resize image using subsample
-    Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
-
-    # Display image in right_frame
-    Label(right_frame, image=image).grid(row=0,column=0, padx=5, pady=5)
-
-# Create tool bar frame
-tool_bar = Frame(left_frame, width=180, height=185)
-tool_bar.grid(row=2, column=0, padx=5, pady=5)
-
-# Example labels that serve as placeholders for other widgets
-Label(tool_bar, text="Tools", width=15).grid(row=0, column=0, padx=5, pady=3)  # ipadx is padding inside the Label widget
-Label(tool_bar, text="Filters", width=15).grid(row=0, column=1, padx=5, pady=3)
-
-# Example labels that could be displayed under the "Tool" menu
-Button(tool_bar, text="Select An Image", command=select, width=15).grid(row=1, column=0, padx=5, pady=5)
-Button(tool_bar, text="Take A Picture", command=picture, width=15).grid(row=2, column=0, padx=5, pady=5)
-
-if(img != None):
-    Button(tool_bar, text="Export", command=export, width=15).grid(row=3, column=0, padx=5, pady=5)
-
-    # Example labels that could be displayed under the "Filter" menu
-    Button(tool_bar, text="Pencil Sketch", command="", width=15).grid(row=1, column=1, padx=5, pady=5)
-    Button(tool_bar, text="Cartoonize", command="", width=15).grid(row=2, column=1, padx=5, pady=5)
-    Button(tool_bar, text="Sephia", command="", width=15).grid(row=3, column=1, padx=5, pady=5)
-    Button(tool_bar, text="Anaglyph", command="", width=15).grid(row=4, column=1, padx=5, pady=5)
-
-else:
-    b1 = Button(tool_bar, text="Export", command=export, width=15)
-    b1['state'] = DISABLED
-    b1.grid(row=3, column=0, padx=5, pady=5)
+def anaglyph():
+    global filter_img, root, ori_img
+    img = np.array(ori_img)
+    filter_img = Image.fromarray(Filter.anaglyph(img))
+    root.destroy()
+    createFrame()
     
-    # Example labels that could be displayed under the "Filter" menu
-    b2 = Button(tool_bar, text="Pencil Sketch", command="", width=15)
-    b2['state'] = DISABLED
-    b2.grid(row=1, column=1, padx=5, pady=5)
-
-    b3 = Button(tool_bar, text="Cartoonize", command="", width=15)
-    b3['state'] = DISABLED
-    b3.grid(row=2, column=1, padx=5, pady=5)
-
-    b4 = Button(tool_bar, text="Sephia", command="", width=15)
-    b4['state'] = DISABLED
-    b4.grid(row=3, column=1, padx=5, pady=5)
-
-    b5 = Button(tool_bar, text="Anaglyph", command="", width=15)
-    b5['state'] = DISABLED
-    b5.grid(row=4, column=1, padx=5, pady=5)
-
-root.mainloop()
+createFrame()
